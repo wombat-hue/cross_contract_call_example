d8217eff28691b39f81b320b0a1c5523e376a30a
Develop <2342342342342@234234232322.com>
2021-05-23 19:49:37 -0700

    Modified tests to be run from assembly directory (non-browser)

--- contract/assembly/__tests__/main.spec.ts
+++ contract/assembly/__tests__/main.spec.ts
@@ -1,57 +1,41 @@
-import { addLongNumbers } from '..';
-import { storage, Context } from "near-sdk-as";
+import * as contract from '..';
+import { storage, Context, VM, VMContext, logging } from "near-sdk-as";
 
-describe("Greeting ", () => {
-    it("adds one digit", async function () {
-        const params = {
-            a: "1",
-            b: "3"
-        };
-        const result = await contract.addLongNumbers(params);
+describe("Calculator", () => {
+    beforeAll(() => {
+        // There can be some common setup for each test.
+        logging.log("Before All")
+    });
+
+    // https://docs.near.org/docs/tutorials/contracts/cross-contract-calls#docsNav
+
+    it("adds one digit", () => {
+        const result = contract.addLongNumbers("1", "3");
         expect(result).toBe("4");
     });
 
-    it("should work with first string longer", async function () {
-        const params = {
-            a: "10",
-            b: "3"
-        };
-        const result = await contract.addLongNumbers(params);
+    it("should work with first string longer", () => {
+        const result = contract.addLongNumbers("10", "3");
         expect(result).toBe("13");
     });
 
-    it("should work with second string longer", async function () {
-        const params = {
-            a: "4",
-            b: "15"
-        };
-        const result = await contract.addLongNumbers(params);
+    it("should work with second string longer", () => {
+        const result = contract.addLongNumbers("4", "15");
         expect(result).toBe("19");
     });
 
-    it("should work with carry", async function () {
-        const params = {
-            a: "19",
-            b: "22"
-        };
-        const result = await contract.addLongNumbers(params);
+    it("should work with carry", () => {
+        const result = contract.addLongNumbers("19", "22");
         expect(result).toBe("41");
     });
 
-    it("should work when result is one digit longer than largest input", async function () {
-        const params = {
-            a: "91",
-            b: "22"
-        };
-        const result = await contract.addLongNumbers(params);
+    it("should work when result is one digit longer than largest input", () => {
+        const result = contract.addLongNumbers("91", "22");
         expect(result).toBe("113");
     });
 
-    it("should work with really large input", async function () {
-        const params = {
-            a: "29348756231984613809465238956138947136497182364018246710289467102946710289467198046",
-            b: "1"
-        };
-        const result = await contract.addLongNumbers(params);
+    it("should work with really large input", () => {
+        const result = contract.addLongNumbers("29348756231984613809465238956138947136497182364018246710289467102946710289467198046", "1");
         expect(result).toBe("29348756231984613809465238956138947136497182364018246710289467102946710289467198047");
     });
+});
